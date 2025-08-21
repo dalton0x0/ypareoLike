b78b3da79fd5127f59ea57b99deb8ba40836eacd
Chéridanh <divintsiela@gmail.com>
2025-08-22 00:55:19 +0200

    first commit after creating project

---
+++ src/test/java/com/ypareo/like/YpareoLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ypareo.like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YpareoLikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
