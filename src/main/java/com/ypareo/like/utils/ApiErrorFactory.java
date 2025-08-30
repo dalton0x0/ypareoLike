c7674be2ec7f658137caec3ace50b15bd976391e
Chéridanh <divintsiela@gmail.com>
2025-08-30 14:36:30 +0200

    chore(exception) : add api error factory component file

--- src/main/java/com/ypareo/like/utils/ApiErrorFactory.java
+++ src/main/java/com/ypareo/like/utils/ApiErrorFactory.java
@@ -1,4 +1,27 @@
 package com.ypareo.like.utils;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
 public class ApiErrorFactory {
+
+    public ResponseEntity<ApiError> buildApiError(Exception ex, HttpStatus status, HttpServletRequest request, List<ApiSubError> subErrors) {
+
+        ApiError error = ApiError.builder()
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .timestamp(LocalDateTime.now())
+                .subErrors(subErrors)
+                .build();
+
+        return ResponseEntity.status(status).body(error);
+    }
 }
