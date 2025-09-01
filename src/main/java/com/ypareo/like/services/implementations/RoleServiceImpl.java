6d570ac2212f41f6c9a3ad4448cb4b6a80e6fb8d
Chéridanh <divintsiela@gmail.com>
2025-09-01 22:01:38 +0200

    feat(role) : update RoleServiceImpl add validation method and refactor code

--- src/main/java/com/ypareo/like/services/implementations/RoleServiceImpl.java
+++ src/main/java/com/ypareo/like/services/implementations/RoleServiceImpl.java
@@ -2,6 +2,8 @@ package com.ypareo.like.services.implementations;
 
 import com.ypareo.like.dtos.RoleRequestDto;
 import com.ypareo.like.dtos.RoleResponseDto;
+import com.ypareo.like.exceptions.BadRequestException;
+import com.ypareo.like.exceptions.ResourceNotFoundException;
 import com.ypareo.like.mappers.RoleMapper;
 import com.ypareo.like.models.sql.Role;
 import com.ypareo.like.repositories.RoleRepository;
@@ -25,22 +27,38 @@ public class RoleServiceImpl implements RoleService {
     }
 
     public RoleResponseDto getRoleById(Long id) {
-        Role existingRole = roleRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Role with id " + id + " not found")
-        );
+        Role existingRole = getExistingRole(id);
         return roleMapper.convertEntityToDto(existingRole);
     }
 
     public RoleResponseDto createRole(RoleRequestDto roleRequestDto) {
+        validateRoleRequestDto(roleRequestDto);
+        checkUniqueConstraints(roleRequestDto);
         Role newRole = roleMapper.convertDtoToEntity(roleRequestDto);
         Role savedRole = roleRepository.save(newRole);
         return roleMapper.convertEntityToDto(savedRole);
     }
 
     public void deleteRole(Long id) {
-        Role existingRole = roleRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Role with id " + id + " not found")
+        Role existingRole = getExistingRole(id);
+        roleRepository.deleteById(existingRole.getId());
+    }
+
+    private Role getExistingRole(Long id) {
+        return roleRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Role with id " + id + " not found")
         );
-        roleRepository.delete(existingRole);
+    }
+
+    private void validateRoleRequestDto(RoleRequestDto roleRequestDto) {
+        if (roleRequestDto.getName() == null) {
+            throw new BadRequestException("Role name cannot be null");
+        }
+    }
+
+    private void checkUniqueConstraints(RoleRequestDto roleRequestDto) {
+        roleRepository.findByName(roleRequestDto.getName()).ifPresent(existingRole -> {
+            throw new BadRequestException("Role " + roleRequestDto.getName() + " already exists");
+        });
     }
 }
