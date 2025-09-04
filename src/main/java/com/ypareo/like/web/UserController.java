128da623feb41b95e9c6ae586b047dab4dba87fe
Chéridanh <divintsiela@gmail.com>
2025-09-04 17:50:23 +0200

    feat(users) : add user service controller

--- src/main/java/com/ypareo/like/web/UserController.java
+++ src/main/java/com/ypareo/like/web/UserController.java
@@ -1,4 +1,45 @@
 package com.ypareo.like.web;
 
+import com.ypareo.like.dtos.UserRequestDto;
+import com.ypareo.like.dtos.UserResponseDto;
+import com.ypareo.like.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("")
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        List<UserResponseDto> allUsers = userService.getAllUsers();
+        return allUsers.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(allUsers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto userRequestDto) {
+        return ResponseEntity.ok(userService.createUser(userRequestDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto) {
+        return ResponseEntity.ok(userService.updateUser(id, userRequestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
