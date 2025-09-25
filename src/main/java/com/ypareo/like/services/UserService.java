535448773dd3b96b20a7da9a17e2560e8389d4bd
Chéridanh <divintsiela@gmail.com>
2025-09-25 16:44:20 +0200

    feat(users-roles) : update user service - add method to add and remove role

--- src/main/java/com/ypareo/like/services/UserService.java
+++ src/main/java/com/ypareo/like/services/UserService.java
@@ -6,9 +6,13 @@ import com.ypareo.like.dtos.UserResponseDto;
 import java.util.List;
 
 public interface UserService {
+
     List<UserResponseDto> getAllUsers();
     UserResponseDto getUserById(Long id);
     UserResponseDto createUser(UserRequestDto userRequestDto);
     UserResponseDto updateUser(Long id, UserRequestDto userRequestDto);
     void deleteUser(Long id);
+
+    UserResponseDto addRoleToUser(Long userId, Long roleId);
+    UserResponseDto removeRoleFromUser(Long userId, Long roleId);
 }
