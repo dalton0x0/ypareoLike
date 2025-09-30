f20fb3241e5376dc14a2fb774c17f2df45068678
Chéridanh <divintsiela@gmail.com>
2025-09-30 18:31:02 +0200

    chore(security) : add custom password encoder file

--- src/main/java/com/ypareo/like/secutiry/CustomPasswordEncoder.java
+++ src/main/java/com/ypareo/like/secutiry/CustomPasswordEncoder.java
@@ -1,4 +1,19 @@
 package com.ypareo.like.secutiry;
 
-public class CustomPasswordEncoder {
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomPasswordEncoder implements PasswordEncoder {
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return BCrypt.hashpw(rawPassword.toString(), BCrypt.gensalt(12));
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return BCrypt.checkpw(rawPassword.toString(), encodedPassword);
+    }
 }
