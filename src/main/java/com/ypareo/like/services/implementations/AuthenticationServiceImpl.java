c72d40640834374d8a096b83e04dd0694f4b827b
Chéridanh <divintsiela@gmail.com>
2025-09-30 18:44:33 +0200

    features(jwt-authentication) : add authentication service impl

--- src/main/java/com/ypareo/like/services/implementations/AuthenticationServiceImpl.java
+++ src/main/java/com/ypareo/like/services/implementations/AuthenticationServiceImpl.java
@@ -3,35 +3,51 @@ package com.ypareo.like.services.implementations;
 import com.ypareo.like.dtos.LoginRequestDto;
 import com.ypareo.like.dtos.LoginResponseDto;
 import com.ypareo.like.repositories.UserRepository;
-import com.ypareo.like.services.AuthService;
+import com.ypareo.like.services.AuthenticationService;
+import com.ypareo.like.services.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthServiceImpl implements AuthService, UserDetailsService {
+public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @Override
     public ResponseEntity<LoginResponseDto> login(LoginRequestDto loginRequestDto) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword()));
-        return null;
-    }
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email).orElseThrow(
-                () -> new UsernameNotFoundException("This user does not exists")
-        );
+            var user = userRepository.findByUsername(loginRequestDto.getUsername()).orElseThrow(
+                    () -> new UsernameNotFoundException("Invalid username or password")
+            );
+
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword())
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            var jwtToken = jwtService.generateToken(user);
+
+            HttpHeaders responseHeaders = new HttpHeaders();
+            responseHeaders.add("Access-Control-Expose-Headers", "Authorization");
+            responseHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken);
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(LoginResponseDto.builder()
+                        .message("Login successful")
+                        .token(jwtToken)
+                        .build()
+                );
     }
 }
