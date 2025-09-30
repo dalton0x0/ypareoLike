668336b30d3d0c9d6293fa38a2214b2886593ecd
Chéridanh <divintsiela@gmail.com>
2025-09-30 18:25:13 +0200

    chore(security) : add security config file

--- src/main/java/com/ypareo/like/config/SecurityConfiguration.java
+++ src/main/java/com/ypareo/like/config/SecurityConfiguration.java
@@ -1,4 +1,39 @@
 package com.ypareo.like.config;
 
+import com.ypareo.like.secutiry.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**").permitAll())
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/users/**").hasAuthority("ADMIN"))
+                .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/roles/**").hasAuthority("ADMIN"))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
