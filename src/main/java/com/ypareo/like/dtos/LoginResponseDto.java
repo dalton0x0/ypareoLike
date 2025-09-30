0c1e5c9fe492a6c6bfafd4b76f8e7e0d20380487
Chéridanh <divintsiela@gmail.com>
2025-09-30 18:27:05 +0200

    feature(jwt-autentication) : add login response dto

--- src/main/java/com/ypareo/like/dtos/LoginResponseDto.java
+++ src/main/java/com/ypareo/like/dtos/LoginResponseDto.java
@@ -1,4 +1,11 @@
 package com.ypareo.like.dtos;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class LoginResponseDto {
+    private String message;
+    private String token;
 }
