15488b9738e1ba816e908aec31e3638621cdf15c
Chéridanh <divintsiela@gmail.com>
2025-09-30 18:45:44 +0200

    features(jwt-authentication) : update authentication controller

--- src/main/java/com/ypareo/like/web/AuthenticationController.java
+++ src/main/java/com/ypareo/like/web/AuthenticationController.java
@@ -1,4 +1,24 @@
 package com.ypareo.like.web;
 
+import com.ypareo.like.dtos.LoginRequestDto;
+import com.ypareo.like.dtos.LoginResponseDto;
+import com.ypareo.like.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return authenticationService.login(loginRequestDto);
+    }
 }
